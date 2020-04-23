25a69444441c2320947b13d0556fcb1615ab3aea
Arif Ikhsanudin <arif.ikhsanudin.developer@gmail.com>
2020-04-23 13:47:55 +0700

    s

---
+++ string/replace.py
@@ -0,0 +1,2 @@
+print('geeks for geeks geeks geeks geeks'.replace('geeks', 'Geeks'))
+print('geeks for geeks geeks geeks geeks'.replace('geeks', 'GeeksforGeeks', 3))
---
+++ string/strip.py
@@ -0,0 +1,2 @@
+print('   Hello world    '.strip())
+print('SpamSpamBaconSpamEggsSpamSpam'.strip('ampS'))
